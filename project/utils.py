e8717d0346aec505376a90ea1e7c5b00de2ab08e
Pg96 <priamo.1701568@studenti.uniroma1.it>
2020-02-14 19:08:54 +0100

    Changed resize method to return images of the exact wanted size

--- project/utils.py
+++ project/utils.py
@@ -58,30 +58,31 @@ def resize_image(img, width=None, height=None, inter=cv.INTER_AREA):
     """
     # initialize the dimensions of the image to be resized and
     # grab the image size
-    dim = None
-    (h, w) = img.shape[:2]
+    # dim = None
+    # (h, w) = img.shape[:2]
 
     # if both the width and height are None, then return the
     # original image
     if width is None and height is None:
         return img
 
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        # dim = (width, int(h * r))
-        dim = (width, round(h * r))
+    # # check to see if the width is None
+    # if width is None:
+    #     # calculate the ratio of the height and construct the
+    #     # dimensions
+    #     r = height / float(h)
+    #     dim = (int(w * r), height)
+    #
+    # # otherwise, the height is None
+    # else:
+    #     # calculate the ratio of the width and construct the
+    #     # dimensions
+    #     r = width / float(w)
+    #     # dim = (width, int(h * r))
+    #     dim = (width, round(h * r))
 
     # resize the image
+    dim = (width, height)
     resized = cv.resize(img, dim, interpolation=inter)
 
     # return the resized image
